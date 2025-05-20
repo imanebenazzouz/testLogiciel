2766d597a4746d73397adf853803ffc092540b67
BENAZZOUZ Imane <12209122@s30418.iutv.univ-paris13.fr>
2025-05-20 16:38:22 +0200

    tp2 statistiqueV2

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -5,10 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.mockito.Mockito.*;
-
-import org.example.service.Statistique;
-import org.example.service.StatistiqueImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
