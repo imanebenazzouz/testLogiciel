36dc4daa4391a4add12a478f59d40e6e85d97ef8
BENAZZOUZ Imane <12209122@s30418.iutv.univ-paris13.fr>
2025-05-20 17:01:21 +0200

    tp3 statistique

--- src/test/java/com/example/demo/web/WebTests.java
+++ src/test/java/com/example/demo/web/WebTests.java
@@ -4,7 +4,6 @@ import com.example.demo.data.Voiture;
 import com.example.demo.service.Echantillon;
 import com.example.demo.service.StatistiqueImpl;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -27,4 +25,32 @@ class WebTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    public void testAjouterVoiture() throws Exception {
+        String voitureJson = "{\"marque\":\"Renault\", \"prix\":20000}";
+
+        mockMvc.perform(post("/voiture")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(voitureJson))
+                .andExpect(status().isOk());
+        verify(statistiqueImpl).ajouter(any(Voiture.class));
+    }
+
+    @Test
+    public void testStatistiqueOK() throws Exception {
+        when(statistiqueImpl.prixMoyen()).thenReturn(new Echantillon(2, 15000));
+
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombreDeVoitures").value(2))
+                .andExpect(jsonPath("$.prixMoyen").value(15000));
+    }
+
+    @Test
+    public void testStatistiquePasDeVoiture() throws Exception {
+        when(statistiqueImpl.prixMoyen()).thenThrow(new ArithmeticException());
+
+        mockMvc.perform(get("/statistique"))
+                .andExpect(status().isNotFound());
+    }
 }
